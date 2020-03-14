f6b8b05f2fba97d51c11849916bc4ea122e9a584
Malik <malikmukhtar63@gmail.com>
2020-03-14 10:02:57 +0100

    counter functionality

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -19,8 +19,12 @@ class Counters extends Component {
          
         return (
             <div>
-                {this.state.counters.map(counter => <Counter key={counter.id} onDelete={this.handleDelete}
-                    value={counter.value} id={counter.id} />)}
+                {this.state.counters.map(counter => (
+                    <Counter key={counter.id}
+                    onDelete={this.handleDelete}
+                    value={counter.value}
+                    id={counter.id}
+                />))}
             </div>
         );
     }
